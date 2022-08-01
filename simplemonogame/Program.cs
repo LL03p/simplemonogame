3833b99cce696395203df30e8e06c0a27dd15336
CAMT <CAMT@DESKTOP-325D3AA>
2022-08-01 10:04:03 +0700

    Add project files.

---
+++ simplemonogame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace simplemonogame
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
